d8f4a9cb10852acc5725cf0aba9e4f278779013c
Winphillips <98627220+Winphillips@users.noreply.github.com>
2022-12-09 13:47:31 -0600

    quine in c

---
+++ quine.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+  char *s = "#include <stdio.h>%c%cint main() {%c  char *s = %c%s%c;%c  printf(s, 10, 10, 10, 34, s, 34, 10, 10);%c  return 0;%c}%c";
+  printf(s, 10, 10, 10, 34, s, 34, 10, 10);
+  return 0;
+}
